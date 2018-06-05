4831bac680e34b2dd7f84b45c08ccfe4408d2963
Christian_Knoll <a1305772@unet.univie.ac.at>
2018-06-05 17:06:55 +0200

    add inline keyboard for choosing the exercise category

--- botInstruction/botCommands.js
+++ botInstruction/botCommands.js
@@ -1,7 +1,7 @@
 const userModel = require("../model/User");
 const helper = require("../helper/helper");
 const userController = require("../controller/userController");
-
+const request = require("request");
 
 class BotCommands {
     constructor(bot, nceToken, baseUrl) {
@@ -41,6 +41,70 @@ class BotCommands {
                 this.bot.sendVideo(gif.from.id, "./media/easter/oZXvR.gif");
                 console.log(gif.from.id);
             });
+
+        this.bot.on('/more', (msg) => {
+
+            let replyMarkup = this.bot.inlineKeyboard([
+                [
+                    this.bot.inlineButton('Choose Category', {callback: 'chooseCat'})
+                ]
+            ], {resize: true});
+
+            return this.bot.sendMessage(msg.from.id, 'What do you want to do?', {replyMarkup});
+
+        });
+
+        this.bot.on('callbackQuery', (msg) => {
+            //this.bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }!`);
+
+            if(msg.data == "chooseCat") {
+                let p1 = new Promise(resolve => {
+                    let options =
+                        {
+                            url: this.baseUrl + "/exercise/category",
+                            json: true,
+                            headers: {
+                                token: this.nceToken
+                            }
+                        };
+                    request.get(options, (error, response, body) => {
+                            if (error)
+                                console.log("get user Throws an error, check");
+
+                            if (!error && response.statusCode === 200) {
+                                //return pretty json
+                                //show first exercise of this category
+                                resolve({body: body});
+                            }
+                        }
+                    )
+
+                });
+                return p1.then((res) => {
+                    let exCategories = res.body.results;
+                    let buttons = [];
+
+                    for(var i = 0; i < exCategories.length; i++) {
+                        //console.log(exCategories[i].name);
+                        let buttonsSub = [];
+                        buttonsSub.push(this.bot.inlineButton(exCategories[i].name, {callback: exCategories[i].name}));
+
+                        buttons.push(buttonsSub);
+                    }
+
+                    let replyMarkup = this.bot.inlineKeyboard(
+                        buttons
+                    , {resize: true});
+
+                    this.bot.sendMessage(msg.from.id, 'Please choose the exercise category:', {replyMarkup});
+                    //this.bot.sendMessage(msg.from.id, String(res.body));
+                    }
+                )
+            } else {
+                this.bot.sendMessage(msg.from.id, `We have to do something here!`);
+            }
+        })
+
     };
 }
 
